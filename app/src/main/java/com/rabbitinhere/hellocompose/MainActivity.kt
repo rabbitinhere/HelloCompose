fb391ad13fa5264cf35307766b30820c712660e6
rabbitinhere <hxq6180078@gmail.com>
2021-09-05 22:11:34 +0800

    按照教程添加界面到Modifier部分

--- app/src/main/java/com/rabbitinhere/hellocompose/MainActivity.kt
+++ app/src/main/java/com/rabbitinhere/hellocompose/MainActivity.kt
@@ -2,10 +2,51 @@ package com.rabbitinhere.hellocompose
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.res.dimensionResource
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContent {
+            MessageCard(Message("Android", "Jetpack Compose"))
+        }
     }
+
+    @Composable
+    fun MessageCard(msg: Message){
+        Row(modifier = Modifier.padding(all = dimensionResource(id = R.dimen.padding))){
+            Image(painter = painterResource(R.drawable.cat11),
+            contentDescription = "Profile picture",
+            modifier = Modifier
+                .size(40.dp)
+                .clip(CircleShape))
+            Spacer(modifier = Modifier.width(8.dp))
+            Column{
+                Text(text = msg.author)
+                Spacer(modifier = Modifier.height(4.dp))
+                Text(text = msg.body)
+            }
+        }
+    }
+
+    @Preview
+    @Composable
+    fun PreviewMessageCard(){
+        MessageCard(
+            msg = Message("Colleague", "Hey, take a look at Jetpack Compose, it's great!")
+        )
+    }
+
+    data class Message(val author: String, val body: String)
 }
