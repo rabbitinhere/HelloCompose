4f3f2a41202ab26c972e132f39a69387fa8905b9
rabbitinhere <hxq6180078@gmail.com>
2021-09-05 14:15:00 +0800

    增加初始项目文件

---
+++ app/src/main/java/com/rabbitinhere/hellocompose/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.rabbitinhere.hellocompose
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
