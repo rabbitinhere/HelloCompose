a71ad837f219ffa39ec5a357899327d9a45f415b
junming.hu <junming.hu@lachesis-mh.com>
2022-01-05 16:09:23 +0800

    增加部分注释

--- app/src/main/java/com/rabbitinhere/hellocompose/IntrinsicSizeActivity.kt
+++ app/src/main/java/com/rabbitinhere/hellocompose/IntrinsicSizeActivity.kt
@@ -37,14 +37,21 @@ class IntrinsicSizeActivity : AppCompatActivity() {
         }
     }
 
+    /**
+     * 这个例子为了让中间的分割线，刚好有两边Text控件那么高。
+     *
+     * 由于在绘制的时候，分割线不知道Text到底有多高。这时候就要用到IntrinsicSize：
+     * 1.Row添加modifier.height(IntrinsicSize.Min): 意思是所有子view要被强制约束高度为他们的固有高度们的最大值
+     *      (固有高度：就是一个控件自己必须要占的高度，比如Text的固有高度取决于他文本多大。下面的分割线由于没给定具体height，所以他的固有高度是0.)
+     * 2.浴室只要让分割线高度是铺满，就铺满到他的父布局Row的高度了，Row的高度由于IntrinsicSize已经被固定了所以就可以达到要求。
+     */
     @Composable
     fun TwoTexts(modifier: Modifier = Modifier, text1: String, text2: String) {
         Row(modifier = modifier.height(IntrinsicSize.Min)) {
             Text(
                 modifier = Modifier
                     .weight(1f)
-                    .padding(start = 4.dp)
-                    .wrapContentWidth(Alignment.Start),
+                    .wrapContentWidth(Alignment.CenterHorizontally),
                 text = text1
             )
 
@@ -52,8 +59,7 @@ class IntrinsicSizeActivity : AppCompatActivity() {
             Text(
                 modifier = Modifier
                     .weight(1f)
-                    .padding(end = 4.dp)
-                    .wrapContentWidth(Alignment.End),
+                    .wrapContentWidth(Alignment.CenterHorizontally),
                 text = text2
             )
         }
